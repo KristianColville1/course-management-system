231a83d763a3ffdb5cf831a4dad59a05800199ff
KristianColville1 <kristiancolville1@gmail.com>
2024-03-22 06:34:28 +0000

    Initial commit

---
+++ src/cms/server/ServerStopException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.server;
+
+/**
+ *
+ * @author kristian
+ * 
+ * Responsible for throwing custom ServerStopException when server fails
+ * to stop
+ */
+public class ServerStopException {
+    
+}
