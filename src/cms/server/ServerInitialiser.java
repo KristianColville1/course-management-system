f0c2caffc72d850f69da357698cd97182cee21a0
KristianColville1 <kristiancolville1@gmail.com>
2024-03-22 06:04:28 +0000

    Added new package server and start orchastrating server for microservices architecture

---
+++ src/cms/server/ServerInitialiser.java
@@ -0,0 +1,25 @@
+package cms.server;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author kristian
+ * 
+ * Responsible for initializing the application server instance 
+ */
+public class ServerInitialiser {
+    // instance fields
+    private int port;
+
+}
