66bf34b56ade43b9a8fba06ccf36b22061b698d4
KristianColville1 <kristiancolville1@gmail.com>
2024-03-22 06:33:33 +0000

    Added constructors for the ServerStartException

--- src/cms/server/ServerStartException.java
+++ src/cms/server/ServerStartException.java
@@ -8,8 +8,25 @@ package cms.server;
  *
  * @author kristian
  * 
- * Responsible for throwing custom exception ServerStartException
+ * Responsible for throwing custom exception ServerStartException when server
+ * fails to start.
  */
 public class ServerStartException extends Exception{
     
+    /**
+     * Default constructor for the ServerStartException
+     */
+    public ServerStartException(){
+        super("Server failed to start. Check configuration and port "
+                + "availablitiy please.");
+    }
+    
+    /**
+     * Constructor for the ServerStartException
+     * @param cause provides more details on the exception to the user
+     */
+    public ServerStartException(Throwable cause){
+        super("Server failed to start due to an exception. "
+                + "See cause for details", cause);
+    }
 }
