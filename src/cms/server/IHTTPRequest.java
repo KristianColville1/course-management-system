ad904d2637d2f07538ddf6c99e83d2a5fe6f7200
KristianColville1 <kristiancolville1@gmail.com>
2024-03-22 07:50:27 +0000

    Begin scuplting http request interface

---
+++ src/cms/server/IHTTPRequest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.server;
+
+/**
+ *
+ * @author kristian
+ *
+ * This interface represents an abstraction over a HTTP request. It provides
+ * access to the common elements of a HTTP request such as headers, parameters
+ * and the request path. This is all done in a server agnostic manner.
+ */
+public interface IHTTPRequest {
+
+}
