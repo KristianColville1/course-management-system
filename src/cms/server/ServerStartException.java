6af3b10e95417a1c58017651621bf483999de170
KristianColville1 <kristiancolville1@gmail.com>
2024-03-22 06:28:35 +0000

    Initial commit

---
+++ src/cms/server/ServerStartException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.server;
+
+/**
+ *
+ * @author kristian
+ * 
+ * Responsible for throwing custom exception ServerStartException
+ */
+public class ServerStartException extends Exception{
+    
+}
