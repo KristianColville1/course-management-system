0abb0d0472c0bb149c3f95ca678dd41a46cec487
KristianColville1 <kristiancolville1@gmail.com>
2024-03-23 13:54:53 +0000

    Implemented getController in Route

--- src/cms/server/Route.java
+++ src/cms/server/Route.java
@@ -18,29 +18,41 @@ public class Route implements IRoute {
     private String path;
     private String method;
     private ControllerBase controller;
-    
+
     /**
      * Constructor for Route. Sets up a Route object with the path, method and
      * controller to use.
-     * 
+     *
      * @param path
      * @param method
-     * @param controller 
+     * @param controller
      */
-    public Route(String path, String method, ControllerBase controller){
+    public Route(String path, String method, ControllerBase controller) {
         this.path = path;
         this.method = method;
         this.controller = controller;
     }
-    
+
     /**
      * Checks if the request path and method equals the routes path and method.
-     * 
+     *
      * @param request is the incoming HTTP request to match against
      * @return true if match otherwise false
      */
     @Override
-    public boolean matches(IHttpRequest request){
-        return request.getMethod().equalsIgnoreCase(method) && request.getPath().equals(path);
+    public boolean matches(IHttpRequest request) {
+        return request.getMethod().equalsIgnoreCase(method)
+                && request.getPath().equals(path);
+    }
+
+    /**
+     * Retrieves the controller associated with the route. Controller handles
+     * the logic for that route.
+     *
+     * @return the controller instance that handles the request
+     */
+    @Override
+    public ControllerBase getController() {
+        return controller;
     }
 }
