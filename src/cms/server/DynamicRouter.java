a1544c80ead1af20942e8b3dbdaa85a0a6bd5691
KristianColville1 <kristiancolville1@gmail.com>
2024-03-23 13:26:18 +0000

    Initial commit

---
+++ src/cms/server/DynamicRouter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author kristian
+ * 
+ * Responsible for dynamically routing requests to the controllers based on 
+ * registered routes here.
+ */
+public class DynamicRouter {
+    // instance fields
+    private List<IRoute> routes = new ArrayList<>();
+    
+}
