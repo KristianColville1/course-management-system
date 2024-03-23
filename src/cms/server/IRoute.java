8c878aa701972c8dba346b544f904c0877f743a0
KristianColville1 <kristiancolville1@gmail.com>
2024-03-23 13:41:53 +0000

    Define how IRoute should be implemented for getting controllers

--- src/cms/server/IRoute.java
+++ src/cms/server/IRoute.java
@@ -5,18 +5,30 @@
 package cms.server;
 
 import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author kristian
+ *
+ * Interface responsible for defining a route in the application. A route is
+ * responsible for matching an incoming request to a controller to it can be
+ * handled correctly.
  */
-
-@FunctionalInterface
 public interface IRoute {
-    
+
     /**
      * Should determine if the route matches a given request.
+     *
      * @param request is the HTTP request to match against
      * @return true if the route matches the request otherwise false
      */
-    boolean matches(HttpServletRequest request);
+    boolean matches(IHttpRequest request);
+
+    /**
+     * Should retrieve the controller associated with this route.
+     *
+     * @return the controller that should handle the request if this route
+     * matches.
+     */
+    ControllerBase getController();
 }
