c1142f78a08dd5e27eff5eac688be609052d2e7b
KristianColville1 <kristiancolville1@gmail.com>
2024-03-23 13:30:54 +0000

    Set up abstract class ControllerBase with handleRequest

--- src/cms/server/ControllerBase.java
+++ src/cms/server/ControllerBase.java
@@ -7,7 +7,21 @@ package cms.server;
 /**
  *
  * @author kristian
+ * 
+ * Abstract base class for all controllers in the application.
+ * It provides a structure for handling HTTP requests and generating responses.
+ * Concrete controllers must implement the handleRequest method to define their
+ * specific behaviour.
  */
-public class ControllerBase {
+public abstract class ControllerBase {
     
+    /**
+     * Handles incoming HTTP requests and generates a response.
+     * 
+     * Concrete subclasses should implement this method to process the request.
+     * 
+     * @param request is the abstracted HTTP request
+     * @param response is the abstracted HTTP response
+     */
+    public abstract void handleRequest(IHttpRequest request, IHttpResponse response);
 }
