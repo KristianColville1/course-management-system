281ba6fead39bf818623ef0764930ff6571c1366
KristianColville1 <kristiancolville1@gmail.com>
2024-03-23 13:33:13 +0000

    Added register router method in dynamic router

--- src/cms/server/ControllerBase.java
+++ src/cms/server/ControllerBase.java
@@ -7,19 +7,19 @@ package cms.server;
 /**
  *
  * @author kristian
- * 
- * Abstract base class for all controllers in the application.
- * It provides a structure for handling HTTP requests and generating responses.
- * Concrete controllers must implement the handleRequest method to define their
- * specific behaviour.
+ *
+ * Abstract base class for all controllers in the application. It provides a
+ * structure for handling HTTP requests and generating responses. Concrete
+ * controllers must implement the handleRequest method to define their specific
+ * behaviour.
  */
 public abstract class ControllerBase {
-    
+
     /**
      * Handles incoming HTTP requests and generates a response.
-     * 
+     *
      * Concrete subclasses should implement this method to process the request.
-     * 
+     *
      * @param request is the abstracted HTTP request
      * @param response is the abstracted HTTP response
      */
--- src/cms/server/DynamicRouter.java
+++ src/cms/server/DynamicRouter.java
@@ -10,12 +10,21 @@ import java.util.List;
 /**
  *
  * @author kristian
- * 
- * Responsible for dynamically routing requests to the controllers based on 
+ *
+ * Responsible for dynamically routing requests to the controllers based on
  * registered routes here.
  */
 public class DynamicRouter {
+
     // instance fields
     private List<IRoute> routes = new ArrayList<>();
-    
+
+    /**
+     * Registers a new route with the router
+     *
+     * @param route is the route to be registered
+     */
+    public void registerRoute(IRoute route) {
+        routes.add(route);
+    }
 }
