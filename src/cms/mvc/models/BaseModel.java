98ed2ccedfedb7de48422481b1e01ebce09a1abb
KristianColville1 <kristiancolville1@gmail.com>
2024-03-24 15:44:11 +0000

    Set up the generateCreateTableStatement method in the abstract BaseModel

--- src/cms/mvc/models/BaseModel.java
+++ src/cms/mvc/models/BaseModel.java
@@ -24,10 +24,10 @@ public abstract class BaseModel {
     /**
      * Static method responsible for skimming through each model that extends
      * this abstract BaseModel and checks for the custom MVC annotations.
-     * 
+     *
      * Uses the annotations to build out the table create statement for each
      * table.
-     * 
+     *
      * @param clazz is the class to build the table statement for
      * @return a SQL CREATE TABLE statement as a String
      */
@@ -36,6 +36,27 @@ public abstract class BaseModel {
             throw new IllegalArgumentException(
                     "The class " + clazz.getSimpleName() + " does not exist");
         }
-        return "";
+
+        Table tableAnnotation = clazz.getAnnotation(Table.class);
+        String tableName = tableAnnotation.name(); // we get the name of table
+
+        List<String> columns = new ArrayList<>(); // for storing column names
+
+        for (Field field : clazz.getDeclaredFields()) { // instance fields
+            if (field.isAnnotationPresent(Column.class)) {
+                Column column = field.getAnnotation(Column.class);
+                // TODO: get column definition 
+            }
+            if (field.isAnnotationPresent(ForeignKey.class)) {
+                ForeignKey foreignKey = field.getAnnotation(ForeignKey.class);
+                // TODO: get the foreign key definition
+            }
+        }
+
+        // combine the column definitions
+        // TODO: the constraints if any also
+        String columnSQL = String.join(", ", columns);
+        return String.format(
+                "CREATE TABLE %s (%s);", tableName, columnSQL);
     }
 }
