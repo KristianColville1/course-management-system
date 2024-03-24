53a209a290301577fb1e6b30f4d7a673037e48c9
KristianColville1 <kristiancolville1@gmail.com>
2024-03-24 17:31:18 +0000

    Added SaltGenerator static util method to generate a random salt

--- src/cms/database/security/SaltGenerator.java
+++ src/cms/database/security/SaltGenerator.java
@@ -4,10 +4,28 @@
  */
 package cms.database.security;
 
+import java.security.SecureRandom;
+import javax.xml.bind.DatatypeConverter;
 /**
  *
  * @author kristian
+ *
+ * Utility class for generating a salt for passwords.
+ *
+ * Generates a cryptographic salt. These are used to add randomness to passwords
+ * before they are hashed to enhance security.
  */
 public class SaltGenerator {
-    
+
+    /**
+     * Generates a new salt.
+     * 
+     * @return hexadecimal representation of a generated salt
+     */
+    public static String generateSalt(){
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16]; // big enough
+        random.nextBytes(salt);
+        return DatatypeConverter.printHexBinary(salt);
+    }
 }
