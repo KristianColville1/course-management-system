399859e090a50c51c958c1301118a896d31a78f4
KristianColville1 <kristiancolville1@gmail.com>
2024-03-24 18:28:06 +0000

    Added fields for building the table statement for users

--- src/cms/apps/auth/models/User.java
+++ src/cms/apps/auth/models/User.java
@@ -15,5 +15,48 @@ import cms.mvc.annotations.Column;
 @Model
 @Table(name = "users")
 public class User {
-    
+
+    // instances
+    /**
+     * Primary key in the user database table
+     */
+    @Column(name = "user_id", type = "INT", primaryKey = true, nullable = false)
+    private int userId;
+
+    /**
+     * The username of a user
+     */
+    @Column(name = "username", type = "VARCHAR(50)", nullable = false, unique = true)
+    private String username;
+
+    /**
+     * The users first name
+     */
+    @Column(name = "first_name", type = "VARCHAR(50)", nullable = false)
+    private String firstName;
+
+    /**
+     * The users last name
+     */
+    @Column(name = "last_name", type = "VARCHAR(50)", nullable = false)
+    private String lastName;
+
+    /**
+     * The users password hash to compare against
+     */
+    @Column(name = "password_hash", type = "CHAR(64)", nullable = false)
+    private String passwordHash;
+
+    /**
+     * The users salt - each user gets their own
+     */
+    @Column(name = "salt", type = "CHAR(16)", nullable = false)
+    private String salt;
+
+    /**
+     * The users role
+     */
+    @Column(name = "role", type = "VARCHAR(20)", nullable = false)
+    private String role;
+
 }
