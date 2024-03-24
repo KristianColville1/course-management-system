2a750e1b00b1ab872a03926234e7c09d89516e06
KristianColville1 <kristiancolville1@gmail.com>
2024-03-24 18:09:56 +0000

    Sculpt the user model and begin testing database integration

---
+++ src/cms/apps/auth/models/User.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.apps.auth.models;
+
+import cms.mvc.annotations.Model;
+import cms.mvc.annotations.Table;
+import cms.mvc.annotations.Column;
+
+/**
+ *
+ * @author kristian
+ */
+@Model
+@Table(name = "users")
+public class User {
+    
+}
