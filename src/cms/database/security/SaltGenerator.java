fa3126bef33146bf6ed9a71d467a1f0542e35267
KristianColville1 <kristiancolville1@gmail.com>
2024-03-24 17:23:47 +0000

    Initial commit

---
+++ src/cms/database/security/SaltGenerator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.database.security;
+
+/**
+ *
+ * @author kristian
+ */
+public class SaltGenerator {
+    
+}
