65aaaa7d1faab9113d38e8eb3f7a71a17fe9b2dc
KristianColville1 <kristiancolville1@gmail.com>
2024-03-24 17:05:15 +0000

    Added default constructor for the AccessControlManger

--- src/cms/database/security/AccessControlManager.java
+++ src/cms/database/security/AccessControlManager.java
@@ -6,18 +6,29 @@ package cms.database.security;
 
 import cms.database.security.enums.Permission;
 import cms.database.security.enums.UserRole;
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  *
  * @author kristian
- * 
+ *
  * Manages access control by mapping user roles to specific permissions. This
- * class provides the functionality to check whether a given role has the 
+ * class provides the functionality to check whether a given role has the
  * necessary permissions to perform an action in the system.
  */
 public class AccessControlManager {
-    
+
     // instance fields
     private final Map<UserRole, EnumSet<Permission>> accessControlMap;
+
+    /**
+     * Default constructor AccessControlManager.
+     *
+     * Sets up the accessControlMap of the various users and their permissions
+     */
+    public AccessControlManager() {
+        accessControlMap = new HashMap<>(); // set it up here
+    }
 }
