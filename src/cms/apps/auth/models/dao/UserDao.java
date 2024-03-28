a9b3465f5cbe8cd7c3d97c411b62c9b107a76562
KristianColville1 <kristiancolville1@gmail.com>
2024-03-28 21:21:07 +0000

    Added method to set up user object from the database

--- src/cms/apps/auth/models/dao/UserDao.java
+++ src/cms/apps/auth/models/dao/UserDao.java
@@ -7,6 +7,7 @@ package cms.apps.auth.models.dao;
 import cms.apps.auth.models.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -18,6 +19,13 @@ import java.sql.SQLException;
  */
 public class UserDao {
 
+    /**
+     * Creates a user from a User object
+     *
+     * @param conn to use for connecting to the database
+     * @param user object to place into the database
+     * @return true if the user has been created otherwise false on exception
+     */
     public boolean createUser(Connection conn, User user) {
         String sql
                 = "INSERT INTO users "
@@ -42,5 +50,35 @@ public class UserDao {
             e.printStackTrace();
             return false;
         }
+    }
+
+    /**
+     * Finds a user by the username given in the database
+     *
+     * @param conn is the connection to the database
+     * @param username is the username to check against
+     * @return user object if found otherwise return null
+     */
+    public User findByUsername(Connection conn, String username) {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, username);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    // sets up the user object from database using setters
+                    User user = new User();
+                    user.setUsername(rs.getString("username"));
+                    user.setFirstName(rs.getString("first_name"));
+                    user.setLastName(rs.getString("last_name"));
+                    user.setSalt(rs.getString("salt"));
+                    user.setHashedPassword(rs.getString("password_hash"));
+                    user.setRole(rs.getString("role"));
+                    return user;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
