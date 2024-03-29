a07984338e4c2711d541f8960444ac8b2974c43f
KristianColville1 <kristiancolville1@gmail.com>
2024-03-29 07:55:32 +0000

    Added jsonwebtoken jar and set up static field for token creation/validation

--- src/cms/server/TokenService.java
+++ src/cms/server/TokenService.java
@@ -4,10 +4,19 @@
  */
 package cms.server;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
 /**
  *
  * @author kristian
+ * 
+ * Responsible for creating and validating Bearer tokens in HTTP requests
  */
 public class TokenService {
     
+    // static instance
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(
+    SignatureAlgorithm.HS256);
 }
