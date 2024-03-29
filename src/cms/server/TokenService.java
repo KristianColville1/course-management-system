98c1790867223e5d7ac063cdfd7b2d7816fa4f1d
KristianColville1 <kristiancolville1@gmail.com>
2024-03-29 08:07:15 +0000

    Built a beaere token generator for a given user ID

--- src/cms/server/TokenService.java
+++ src/cms/server/TokenService.java
@@ -4,8 +4,10 @@
  */
 package cms.server;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import java.util.Date;
 
 import javax.crypto.SecretKey;
 /**
@@ -16,7 +18,26 @@ import javax.crypto.SecretKey;
  */
 public class TokenService {
     
-    // static instance
+    // static instances - bearer key and expiration time 
     private static final SecretKey SECRET_KEY = Keys.secretKeyFor(
     SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 86400000; // 1 day
+    
+    /**
+     * Static method generates a new Bearer Token for the user ID given
+     * @param userId is the user this bearer belongs to
+     * @return the Bearer token header string
+     */
+    public static String generateBearerToken(int userId){
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+        
+        String token = Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+        return "Bearer " + token;
+    }
 }
