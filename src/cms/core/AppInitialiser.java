cc4a29b233c262a2df4c64a1aba017664858ead1
KristianColville1 <kristiancolville1@gmail.com>
2024-03-30 16:52:47 +0000

    Start refactoring application logic

---
+++ src/cms/core/AppInitialiser.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.core;
+
+/**
+ *
+ * @author kristian
+ * 
+ * Responsible for initialising the application and running services.
+ */
+public class AppInitialiser {
+
+    /**
+     * Starts the application services
+     */
+    public void startServices() {
+    
+    }
+
+}
