51f48a570e9bc8756094d669ea630ced4697b80a
KristianColville1 <kristiancolville1@gmail.com>
2024-03-30 20:35:55 +0000

    Overridden the render implementation for BaseView

--- src/cms/mvc/views/BaseView.java
+++ src/cms/mvc/views/BaseView.java
@@ -4,13 +4,32 @@
  */
 package cms.mvc.views;
 
+import cms.server.IHttpRequest;
+import cms.server.IHttpResponse;
+
 /**
  *
  * @author kristian
- * 
- * Concrete classes which extend this BaseView class should carry out 
- * behaviour defined here in a structured and predictable way.
+ *
+ * Concrete classes which extend this BaseView class should carry out behaviour
+ * defined here in a structured and predictable way.
  */
-public abstract class BaseView implements IView{
-    
+public abstract class BaseView implements IView {
+
+    /**
+     * Implementation for the IView, child classes will use this on render to
+     * perform the logic views should carry out in a structured and predictable
+     * way.
+     * 
+     * When called in child classes it will automatically call methods required
+     * in the order they are needed.
+     *
+     * @param request is the IHttpRequest object
+     * @param response is the IHttpResponse object
+     */
+    @Override
+    public void render(IHttpRequest request, IHttpResponse response) {
+
+    }
+
 }
