bc5cbe1acbeecb399d0973021761116544dc8c4b
KristianColville1 <kristiancolville1@gmail.com>
2024-03-30 20:27:41 +0000

    Initial commit

---
+++ src/cms/mvc/views/BaseView.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms.mvc.views;
+
+/**
+ *
+ * @author kristian
+ * 
+ * Concrete classes which extend this BaseView class should carry out 
+ * behaviour defined here in a structured and predictable way.
+ */
+public abstract class BaseView implements IView{
+    
+}
