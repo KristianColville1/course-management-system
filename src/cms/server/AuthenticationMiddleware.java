3b311b6f2103a00d61b7d07235ef238ad45572db
KristianColville1 <kristiancolville1@gmail.com>
2024-03-30 14:04:33 +0000

    Added constructor to config a home route for auth fails

--- src/cms/server/AuthenticationMiddleware.java
+++ src/cms/server/AuthenticationMiddleware.java
@@ -9,18 +9,31 @@ import java.io.IOException;
 /**
  *
  * @author kristian
- * 
- * Responsible for handling the authentication of requests and implements the 
+ *
+ * Responsible for handling the authentication of requests and implements the
  * IMiddleware interface.
- * 
+ *
  * Processes requests for authentication.
- * 
+ *
  */
 public class AuthenticationMiddleware implements IMiddleware {
+
+    // instance fields
+    private final String homeRoute; // whatever is home
+
+    /**
+     * Constructor for AuthenticationMiddleware responsible for using dependency
+     * injection to configure the home route of the micro service.
+     *
+     * @param homeRoute is the route to bring user home if auth fails
+     */
+    public AuthenticationMiddleware(String homeRoute) {
+        this.homeRoute = homeRoute;
+    }
 
     @Override
     public boolean process(IHttpRequest request, IHttpResponse response) throws IOException {
         return false;
     }
-    
+
 }
