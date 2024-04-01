3a39c46c45446a9fdfbc6d2b0eb92540977d02b5
KristianColville1 <kristiancolville1@gmail.com>
2024-04-01 19:45:20 +0100

    Added constructor for the input handler

--- src/cms/utils/InputHandler.java
+++ src/cms/utils/InputHandler.java
@@ -4,10 +4,23 @@
  */
 package cms.utils;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kristian
+ * 
+ * InputHandler is responsible for managing input
  */
 public class InputHandler {
-    
+
+    // instance fields
+    private final Scanner scanner;
+
+    /**
+     * Constructor for the InputHandler sets up the scanner for user input
+     */
+    public InputHandler() {
+        this.scanner = new Scanner(System.in);
+    }
 }
