6d4d5d8678e49a36991040785f562e85251778c7
KristianColville1 <kristiancolville1@gmail.com>
2024-04-01 14:43:59 +0100

    Refactored the app init to go straight to home menu

--- src/cms/core/AppInitialiser.java
+++ src/cms/core/AppInitialiser.java
@@ -5,15 +5,20 @@
 package cms.core;
 
 import cms.database.DatabaseInitialiser;
+import cms.server.IHttpRequest;
+import cms.server.InternalRequestHandler;
 import cms.server.ServerInitialiser;
+import cms.server.SimpleHttpRequest;
 
 /**
  *
  * @author kristian
- * 
+ *
  * Responsible for initialising the application and running services.
  */
 public class AppInitialiser {
+
+    private ServerInitialiser serverInitialiser;
 
     /**
      * Starts the application services
@@ -26,25 +31,43 @@ public class AppInitialiser {
     /**
      * Responsible for setting up the database
      */
-    private void databaseInit(){
+    private void databaseInit() {
         DatabaseInitialiser db = new DatabaseInitialiser();
         db.initialise();
-
     }
-    
+
     /**
      * Responsible for setting up the server.
-     * 
+     *
      * Sets up server on its own thread
      */
-    private void serverInit(){
-        ServerInitialiser serverInitialiser = new ServerInitialiser(0);
+    private void serverInit() {
 
         // Starting the server in its own thread
         Thread serverThread = new Thread(() -> {
+            serverInitialiser = new ServerInitialiser(0);
+            serverInitialiser.setOnReadyListener(() -> startMenu());
             serverInitialiser.start();
         });
 
         serverThread.start();
+
+    }
+
+    /**
+     * Sets up the initial internal routing and sets up start menu for user
+     */
+    private void startMenu() {
+        int portToRouteTo = serverInitialiser.getRealPortNumber();
+        System.out.println(portToRouteTo);
+        IHttpRequest request = new SimpleHttpRequest(
+                "http",
+                "localhost",
+                portToRouteTo,
+                "GET",
+                "/");
+        InternalRequestHandler.sendRequest(request,
+                "/",
+                "GET");
     }
 }
