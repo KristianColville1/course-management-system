4c0e56870d4d6494b169eef24343b9be50955266
KristianColville1 <kristiancolville1@gmail.com>
2024-04-02 19:17:10 +0100

    Extended base view and begin building login portal

--- src/cms/apps/auth/controllers/AuthController.java
+++ src/cms/apps/auth/controllers/AuthController.java
@@ -4,10 +4,23 @@
  */
 package cms.apps.auth.controllers;
 
+import cms.apps.auth.views.AuthLoginView;
+import cms.mvc.views.IView;
+import cms.server.ControllerBase;
+import cms.server.IHttpRequest;
+import cms.server.IHttpResponse;
+import cms.server.annotations.Route;
+
 /**
  *
  * @author kristian
  */
-public class AuthLoginController {
+public class AuthController extends ControllerBase{
+
+    @Override
+    @Route(path="/login", method="GET")
+    public void handleRequest(IHttpRequest request, IHttpResponse response) {
+        IView view = new AuthLoginView();
+    }
     
 }
--- src/cms/apps/auth/views/AuthLoginView.java
+++ src/cms/apps/auth/views/AuthLoginView.java
@@ -4,10 +4,35 @@
  */
 package cms.apps.auth.views;
 
+import cms.mvc.views.BaseView;
+import cms.server.IHttpRequest;
+import cms.server.IHttpResponse;
+
 /**
  *
  * @author kristian
  */
-public class AuthLoginView {
+public class AuthLoginView extends BaseView{
+    private final int maxSelectionValue;
+    
+    public AuthLoginView(IHttpRequest request, IHttpResponse response) {
+        super(request, response);
+        maxSelectionValue = 3;
+    }
+
+    @Override
+    protected void buildOptionsMap() {
+        
+    }
+
+    @Override
+    protected void renderOptions() {
+        
+    }
+
+    @Override
+    protected void routeToNextView(int userInput) throws IllegalArgumentException {
+        
+    }
     
 }
